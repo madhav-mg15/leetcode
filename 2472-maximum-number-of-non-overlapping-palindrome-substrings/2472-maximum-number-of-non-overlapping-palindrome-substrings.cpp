5e1337d6dd171aa2a86a404fa1e393223f1fb812
madhav-mg15 <madhavgupta727@gmail.com>
2026-09-15 08:09:32 +0530

    Time: 115 ms (63.35%), Space: 9.6 MB (79.06%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -3,61 +3,112 @@ int n,k;
 vector<int> dp;
 class Solution {
 public:
-    int f(int i){
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ans=0,fl=0;
-        for(int j=i;j<n;j++){
-            int l=j, r=j;
-            while(l>=i && r<n && s[l]==s[r]){
-                int len=r-l+1;
-                if(len==k){
-                    ans=max(ans,1+f(r+1));
-                    fl=k-1;
-                    break;
-                }
-                if(len==k+1){
-                    ans=max(ans,1+f(r+1));
-                    fl=k;
-                    break;
-                }
-                if(len>k+1){
-                    fl=0;
-                    break;
-                }
-                l--;
-                r++;
-            }
-            if(fl) continue;
-            l=j, r=j+1,fl=0;
-            while(l>=i && r<n && s[l]==s[r]){
-                int len=r-l+1;
-                if(len==k){
-                    ans=max(ans,1+f(r+1));
-                    fl=k-1;
-                    break;
-                }
-                if(len==k+1){
-                    ans=max(ans,1+f(r+1));
-                    fl=k;
-                    break;
-                }
-                if(len>k+1){
-                    fl=0;
-                    break;
-                }
-                l--;
-                r++;
-            }
-            j+=fl;
-        }
-        if(ans==0) ans=max(ans,f(i+1));
-        return dp[i]=ans;
-    }
+    // int f(int i){
+    //     if(i>=n) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int ans=0,fl=0;
+    //     for(int j=i;j<n;j++){
+    //         int l=j, r=j;
+    //         while(l>=i && r<n && s[l]==s[r]){
+    //             int len=r-l+1;
+    //             if(len==k){
+    //                 ans=max(ans,1+f(r+1));
+    //                 fl=k-1;
+    //                 break;
+    //             }
+    //             if(len==k+1){
+    //                 ans=max(ans,1+f(r+1));
+    //                 fl=k;
+    //                 break;
+    //             }
+    //             if(len>k+1){
+    //                 fl=0;
+    //                 break;
+    //             }
+    //             l--;
+    //             r++;
+    //         }
+    //         if(fl) continue;
+    //         l=j, r=j+1,fl=0;
+    //         while(l>=i && r<n && s[l]==s[r]){
+    //             int len=r-l+1;
+    //             if(len==k){
+    //                 ans=max(ans,1+f(r+1));
+    //                 fl=k-1;
+    //                 break;
+    //             }
+    //             if(len==k+1){
+    //                 ans=max(ans,1+f(r+1));
+    //                 fl=k;
+    //                 break;
+    //             }
+    //             if(len>k+1){
+    //                 fl=0;
+    //                 break;
+    //             }
+    //             l--;
+    //             r++;
+    //         }
+    //         j+=fl;
+    //     }
+    //     if(ans==0) ans=max(ans,f(i+1));
+    //     return dp[i]=ans;
+    // }
     int maxPalindromes(string s1, int k1) {
         s=s1, k=k1;
         n=s.length();
-        dp = vector<int> (n+1,-1);
-        return f(0);
+        // dp = vector<int> (n+1,-1);
+        dp = vector<int> (n+1,0);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int ans=0;
+            for(int j=i;j<n;j++){
+                int l=j,r=j,fl=0;
+                while(l>=i && r<n && s[l]==s[r]){
+                    int len=r-l+1;
+                    if(len==k){
+                        ans=max(ans,1+dp[r+1]);
+                        fl=k-1;
+                        break;
+                    }
+                    if(len==k+1){
+                        ans=max(ans,1+dp[r+1]);
+                        fl=k;
+                        break;
+                    }
+                    if(len>k){
+                        fl=0;
+                        break;
+                    }
+                    l--;
+                    r++;
+                }
+                if(fl) continue;
+                l=j,r=j+1,fl=0;
+                while(l>=i && r<n && s[l]==s[r]){
+                    int len=r-l+1;
+                    if(len==k){
+                        ans=max(ans,1+dp[r+1]);
+                        fl=k-1;
+                        break;
+                    }
+                    if(len==k+1){
+                        ans=max(ans,1+dp[r+1]);
+                        fl=k;
+                        break;
+                    }
+                    if(len>k){
+                        fl=0;
+                        break;
+                    }
+                    l--;
+                    r++;
+                }
+                j+=fl;
+            }
+            if(ans==0) ans=max(ans,dp[i+1]);
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
