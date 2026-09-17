c18fcc50cc96de4f2db3060255d26745e5ada186
madhav-mg15 <madhavgupta727@gmail.com>
2026-09-17 14:11:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (86.73%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,22 +1,30 @@
 using ll = long long;
-vector<vector<vector<ll>>> dp;
-int n,k; 
+vector<ll> fact; 
 const ll MOD = 1000000007;
 class Solution {
 public:
-    ll f(int i, int j, int t){
-        if(i==n) return j==k && t==0;
-        if(dp[i][j][t]!=-1) return dp[i][j][t];
-        ll ans=f(i+1,j,t)%MOD;
-        if(t==0) ans = (ans%MOD + f(i+1,j,1)%MOD);
-        else{
-            if(j<k) ans = (ans%MOD + f(i+1,j+1,0)%MOD + f(i+1,j+1,1)%MOD)%MOD;
-        }
-        return dp[i][j][t]=ans%MOD;
+    void generate(){
+        static bool flg=false;
+        if(flg) return;
+        flg=true;
+        fact.push_back(1);
+        fact.push_back(1);
+        for(int i=2;i<=2000;i++) fact.push_back((i%MOD * fact[i-1]%MOD)%MOD);
+        return;
     }
-    int numberOfSets(int n1, int k1) {
-        n=n1, k=k1;
-        dp = vector<vector<vector<ll>>> (n+1, vector<vector<ll>>(k+1, vector<ll>(2,-1)));
-        return f(0,0,0)%MOD;
+    ll power(ll x, ll n){
+        if(n==0) return 1;
+        if(n==1) return x%MOD;
+        ll ans=power(x,n/2)%MOD;
+        if(n%2==0) return (ans%MOD * ans%MOD)%MOD;
+        else return (x%MOD * ans%MOD * ans%MOD)%MOD;
+    }
+    int numberOfSets(int n, int k) {
+        generate();
+        ll a=fact[n+k-1];
+        ll b=fact[2*k];
+        ll c=fact[n-k-1];
+        ll d=(b%MOD * c%MOD)%MOD;
+        return (a%MOD * (power(d,MOD-2)%MOD))%MOD;
     }
 };
